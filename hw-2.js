0a6f9253e3a2a995506b2d418015ac67eab99778
ulchaz <uliaesaulkova4@gmail.com>
2024-01-26 18:37:27 +0300

    задание 4

--- hw-2.js
+++ hw-2.js
@@ -10,3 +10,10 @@ alert("Год выпуска первого iPhone - " + year)
 let name = "Брендан Эйх";
 alert("Имя создателя языка JavaScript - " + name)
 
+let m = 10;
+let n = 2;
+alert("сумма - " + (m + n));
+alert("разность - " + (m - n));
+alert("произведение - " + (m * n));
+alert("частное - " + (m / n));
+
