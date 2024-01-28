456267326857fa01a3593fd96c5e63dce21afd89
ulchaz <uliaesaulkova4@gmail.com>
2024-01-28 20:08:56 +0300

    задание 5

--- hw-3.js
+++ hw-3.js
@@ -17,6 +17,8 @@ else{
     console.log('Неверно')
 }
 
+
+
 let d = prompt('Введите число');
 let e = prompt('Введите еще число');
 if (d > 100 || e > 100){
@@ -26,6 +28,46 @@ else{
     alert('Неверно')
 }
 
+
+
+
 let m = '2';
 let n = '3';
 alert((Number(m)) +(Number(n) ));
+
+
+
+let monthNumber = 98;
+let season;
+
+if( monthNumber > 12) {
+  console.log("некорректный номер месяца");
+} else {
+  switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+      console.log('Зима');
+      break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+      break;Весна
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето');
+      break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень');
+      break;
+    default:
+      console.log("некорректный номер месяца");
+  }
+
+
+}
+
