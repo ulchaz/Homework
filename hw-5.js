3a2bb5401af7b8585a878dc510d2aa079f71cf66
ulchaz <uliaesaulkova4@gmail.com>
2024-02-02 23:20:08 +0300

    задание 2

--- hw-5.js
+++ hw-5.js
@@ -11,3 +11,15 @@ function minNum (num1, num2) {
 console.log(minNum(8,4));
 console.log(minNum(6,6));
 
+//задание 2
+
+function evenNum (num) {
+    if (num % 2 == 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+
+console.log(evenNum(8));
+console.log(evenNum(5));
