eee685dd6515b11c4ad0a9ba9d4d9ed1b9fdecd6
ulchaz <uliaesaulkova4@gmail.com>
2024-02-02 23:12:25 +0300

    задание 1

---
+++ hw-5.js
@@ -0,0 +1,13 @@
+//задание 1
+
+function minNum (num1, num2) {
+    if (num1 > num2) {
+        return num2;
+    } else {
+        return num1;
+    }
+}
+
+console.log(minNum(8,4));
+console.log(minNum(6,6));
+
