71eff0b37372479a68ea6af62d4eaf32519186b3
ulchaz <uliaesaulkova4@gmail.com>
2024-02-02 23:35:59 +0300

    задание 3

--- hw-5.js
+++ hw-5.js
@@ -23,3 +23,27 @@ function evenNum (num) {
 
 console.log(evenNum(8));
 console.log(evenNum(5));
+
+
+//задание 3
+
+//3.1
+
+function printNum (number) {
+    console.log(number * number);
+}
+
+printNum(5);
+
+//3.2
+
+function returnNum (number) {
+    return number * number;
+}
+
+printNum(5);
+
+console.log(returnNum(5));
+
+
+
