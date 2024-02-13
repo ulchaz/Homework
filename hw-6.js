de0ebd9c5a630fd6941531c901370ec7fc76d1a9
ulchaz <uliaesaulkova4@gmail.com>
2024-02-13 19:45:06 +0300

    задание 4

--- hw-6.js
+++ hw-6.js
@@ -14,3 +14,20 @@ for (let i = 0; i < num.length; i++) {
 const index = [1, 5, 4, 10, 0, 3]
 
 console.log(index.indexOf(4))
+
+//задание 3 
+
+const joinNum = [1, 3, 5, 10, 20];
+
+console.log(joinNum.join(' '));
+
+//задание 4
+
+let arr = [];
+for (let m = 0; m < 3; m++) {
+  arr[m] = [];
+  for (let j = 0; j < 3; j++) {
+    arr[m][j] = 1;
+  }
+}
+console.log(arr);
