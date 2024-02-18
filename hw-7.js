8f4a3962387524211ae2f6aeb25eb8c2b91a764d
ulchaz <uliaesaulkova4@gmail.com>
2024-02-18 17:55:49 +0300

    задание 2

--- hw-7.js
+++ hw-7.js
@@ -3,3 +3,15 @@
 let str = 'js';
 console.log(str.toUpperCase()); 
 
+//задание 2
+
+
+function searchStart(arr, str) {
+    const result = arr.filter(item => item.toLowerCase().startsWith(str.toLowerCase()));
+    return result;
+}
+
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['Кошка', 'Комар']
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
+
