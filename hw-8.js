0fcea7421f5877257a094d31ca94484aef01f242
ulchaz <uliaesaulkova4@gmail.com>
2024-02-26 21:13:37 +0300

    задание 2

--- hw-8.js
+++ hw-8.js
@@ -8,3 +8,35 @@ const people = [
  ];
  
  console.log(people.sort((a, b) => a.age - b.age));
+
+
+ //задание 2
+
+ function isPositive(num) {
+    return num > 0;
+  }
+  
+  function isMale(person) {
+    return person.gender === 'male';
+  }
+  
+  function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (ruleFunction(arr[i])) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+  
+  console.log(filter([3, -4, 1, 9], isPositive)); 
+  
+  const human = [
+     {name: 'Глеб', gender: 'male'},
+     {name: 'Анна', gender: 'female'},
+     {name: 'Олег', gender: 'male'},
+     {name: 'Оксана', gender: 'female'}
+  ];
+  
+  console.log(filter(human, isMale)); 
