3ec9dc3702b4fefaaa8b9386bdcbbefb2921bad4
ulchaz <156938003+ulchaz@users.noreply.github.com>
2024-02-28 10:15:32 +0300

    Add files via upload

---
+++ hw-8.js
@@ -0,0 +1,94 @@
+//задание 1
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ 
+ console.log(people.sort((a, b) => a.age - b.age));
+
+
+ //задание 2
+
+ function isPositive(num) {
+    return num > 0;
+  }
+  
+  function isMale(person) {
+    return person.gender === 'male';
+  }
+  
+  function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (ruleFunction(arr[i])) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+  
+  console.log(filter([3, -4, 1, 9], isPositive)); 
+  
+  const human = [
+     {name: 'Глеб', gender: 'male'},
+     {name: 'Анна', gender: 'female'},
+     {name: 'Олег', gender: 'male'},
+     {name: 'Оксана', gender: 'female'}
+  ];
+  
+  console.log(filter(human, isMale)); 
+
+  //задание 3
+
+  function printCurrentDate() {
+    const currentDate = new Date();
+    console.log(currentDate);
+  }
+  
+  let counter = 0;
+  const intervalId = setInterval(() => {
+    printCurrentDate();
+    counter += 3;
+    if (counter >= 30) {
+      clearInterval(intervalId);
+      console.log("30 секунд прошло");
+    }
+  }, 3000);
+  
+  setTimeout(() => {
+    clearInterval(intervalId);
+  }, 30000);
+  
+
+  //задание 4
+
+  function delayForSecond(callback) {
+    // Код писать можно только внутри этой функции
+  setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+  console.log('Привет, Глеб!');
+})
+
+//задание 5
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) { cb(); }
+    }, 1000)
+}
+
+
+function sayHi(names) {
+    return function() {
+        console.log('Привет, ' + names + '!');
+    };
+}
+
+const greet = sayHi('Глеб'); 
+delayForSecond(greet);
